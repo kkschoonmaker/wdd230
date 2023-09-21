a0016074be231ed8eddbd8d97e762e9e2b1e120b
kkschoonmaker <kellykschoonmaker@gmail.com>
2023-09-21 01:04:10 -0600

    design principles

---
+++ lesson02/scripts/getdates.js
@@ -0,0 +1,10 @@
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("year").innerHTML = `&copy ${year} | Kelly Schoonmaker`;
+
+let text = document.lastModified;
+document.getElementById(
+  "lastModified"
+).innerHTML = `Last Modification: ${text}`;
+
+//alert(document.lastModified);
