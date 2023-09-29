2a0836f0db8ee570d47b4fbdae0acb3324853daf
kkschoonmaker <kellykschoonmaker@gmail.com>
2023-09-29 00:02:16 -0600

    siteplan

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,7 +2,7 @@ const d = new Date();
 let year = d.getFullYear();
 document.getElementById(
   "year"
-).innerHTML = `&copy ${year} <br> Kelly Schoonmaker <br> Colorado, USA`;
+).innerHTML = `&copy ${year} Kelly Schoonmaker Colorado, USA`;
 
 let text = document.lastModified;
 document.getElementById(
