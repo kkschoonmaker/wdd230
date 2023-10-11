a75e9ac3724f20bae07063b3daaa7ab1b55b52d6
kkschoonmaker <kellykschoonmaker@gmail.com>
2023-10-11 12:21:17 -0600

    .

--- lesson05/scripts/list.js
+++ lesson05/scripts/list.js
@@ -1,19 +1,20 @@
-const input = document.querySelector("#favchap");
-const button = document.querySelector("button");
-const list = document.querySelector("#list");
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
 
-button.addEventListener("click", () => {
-  if (input.value != "") {
-    const li = document.createElement("li");
-    const deleteButton = document.createElement("button");
+button.addEventListener('click', () => {
+  if (input.value != '') {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
     li.textContent = input.value;
-    deleteButton.textContent("❌");
+    deleteButton.textContent = '❌';
     li.append(deleteButton);
     list.append(li);
-    deleteButton.addEventListener("click", () => {
+
+    deleteButton.addEventListener('click', () => {
       list.removeChild(li);
       input.focus();
     });
+    input.value = '';
   }
-  input.value = "";
 });
