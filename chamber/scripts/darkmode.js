c315782eb3b9e72a24a8cad8a35dc214a2fdf2c1
kkschoonmaker <kellykschoonmaker@gmail.com>
2023-10-12 22:33:32 -0600

    .

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,14 +1,17 @@
 const modeButton = document.querySelector('#mode');
 const main = document.querySelector('body');
+const changeText = document.querySelector('.plain-text');
 
 modeButton.addEventListener('click', () => {
   if (modeButton.textContent.includes('Light Mode')) {
     main.style.background = '#f2f2f2';
     main.style.color = '#023373';
+    changeText.style.color = '#023373';
     modeButton.textContent = 'Dark Mode';
   } else {
     main.style.background = '#023373';
     main.style.color = '#023373';
+    changeText.style.color = '#f2f2f2';
     modeButton.textContent = 'Light Mode';
   }
 });
