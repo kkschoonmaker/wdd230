8d243d2fd249ef8331e9c7b8593a852206b4ee4c
kkschoonmaker <kellykschoonmaker@gmail.com>
2023-10-27 11:12:09 -0600

    .

--- chamber/scripts/visits.js
+++ chamber/scripts/visits.js
@@ -1,22 +1,3 @@
-// // Initialize display element variable
-// const visitsDisplay = document.querySelector('.visits');
-
-// // Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
-// let numVisits = Number(window.localStorage.getItem('numVisits-ls')) || 0;
-
-// //  Determine if this is the first visit or display the number of visits. We wrote this example backwards in order for you to think deeply about the logic.
-// if (numVisits !== 0) {
-//   visitsDisplay.textContent = numVisits;
-// } else {
-//   visitsDisplay.textContent = `Welcome! Let us us know if you have any questions.`;
-// }
-
-// //  increment the number of visits by one.
-// numVisits++;
-
-// // store the new visit total into localStorage, key=numVisits-ls
-// localStorage.setItem('numVisits-ls', numVisits);
-
 // milliseconds to days constant = 1000 ms/s * 60 s/m * 60 m/h * 24 h/day
 const msToDays = 84600000;
 let theDateToday = Date.now();
