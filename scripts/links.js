bbf3e28a6986941002561403da78977fa31f49e2
kkschoonmaker <kellykschoonmaker@gmail.com>
2023-11-09 00:08:25 -0700

    .

--- scripts/links.js
+++ scripts/links.js
@@ -5,12 +5,29 @@ const activityLinks = document.querySelector('#activity-links');
 async function getLinks() {
   const response = await fetch(linksURL);
   const data = await response.json();
-  console.table(data);
-  // displayLinks(data);
+  // console.log(data);
+  displayLinks(data.lessons);
 }
 
-// const displayLinks = (weeks) => {
-//   weeks
-// };
+const displayLinks = (lessons) => {
+  lessons.forEach((lesson) => {
+    const linkList = document.createElement('ul');
+    const listItem = document.createElement('li');
+    listItem.textContent = `Week ${lesson.lesson}: `;
+
+    lesson.links.forEach((link) => {
+      const anchor = document.createElement('a');
+      anchor.href = `${link.url}`;
+      anchor.textContent = `${link.title}   |  `;
+
+      anchor.setAttribute('target', 'blank');
+
+      listItem.appendChild(anchor);
+      linkList.appendChild(listItem);
+    });
+
+    activityLinks.appendChild(linkList);
+  });
+};
 
 getLinks();
