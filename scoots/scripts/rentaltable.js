369125c135392e91af9d64cc640717a83d382ad5
kkschoonmaker <kellykschoonmaker@gmail.com>
2023-12-01 01:22:41 -0700

    .

--- scoots/scripts/rentaltable.js
+++ scoots/scripts/rentaltable.js
@@ -3,9 +3,22 @@ const membersURL =
   'https://kkschoonmaker.github.io/wdd230/scoots/data/rentals.json';
 const rentalTable = document.querySelector('#rentalTable');
 
+
 async function getRentals() {
   const response = await fetch(membersURL);
   const data = await response.json();
-  //   console.log(data);
+  console.log(data);
   displayRentals(data.rentals);
 }
+
+const displayRentals = (rentals) => {
+  rentals.forEach((rental) => {
+    const tableBody = document.querySelector('#rentalTable tbody');
+
+    const row = document.createElement('tr');
+    row.innerHTML = `<td>${rental.RentalType}</td><td>${rental.MaxPersons}</td><td>${rental.ReservationPrice[0].FullDay}</td>td>${rental.ReservationPrice[0].HalfDay}</td>td>${rental.WalkinPrice[0].FullDay}</td>td>${rental.WalkinPricePrice[0].HalfDay}</td>`;
+    tableBody.appendChild(row);
+  });
+};
+
+getRentals();
