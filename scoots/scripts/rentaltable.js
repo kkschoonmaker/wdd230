d8a0c8b83f41940a29852d83fc814b918e8f59d8
kkschoonmaker <kellykschoonmaker@gmail.com>
2023-12-05 00:42:23 -0700

    Reservation page

--- scoots/scripts/rentaltable.js
+++ scoots/scripts/rentaltable.js
@@ -1,13 +1,12 @@
 const baseURL = 'https://kkschoonmaker.github.io/wdd230/scoots';
-const membersURL =
+const rentalsURL =
   'https://kkschoonmaker.github.io/wdd230/scoots/data/rentals.json';
 const rentalTable = document.querySelector('#rentalTable');
 
-
 async function getRentals() {
-  const response = await fetch(membersURL);
+  const response = await fetch(rentalsURL);
   const data = await response.json();
-  console.log(data);
+  // console.log(data);
   displayRentals(data.rentals);
 }
 
@@ -16,7 +15,7 @@ const displayRentals = (rentals) => {
     const tableBody = document.querySelector('#rentalTable tbody');
 
     const row = document.createElement('tr');
-    row.innerHTML = `<td>${rental.RentalType}</td><td>${rental.MaxPersons}</td><td>${rental.ReservationPrice[0].FullDay}</td>td>${rental.ReservationPrice[0].HalfDay}</td>td>${rental.WalkinPrice[0].FullDay}</td>td>${rental.WalkinPricePrice[0].HalfDay}</td>`;
+    row.innerHTML = `<td>${rental.RentalType}</td><td>${rental.MaxPersons}</td><td>${rental.ReservationPrice[0].FullDay}</td><td>${rental.ReservationPrice[0].HalfDay}</td><td>${rental.WalkinPrice[0].FullDay}</td><td>${rental.WalkinPrice[0].HalfDay}</td>`;
     tableBody.appendChild(row);
   });
 };
